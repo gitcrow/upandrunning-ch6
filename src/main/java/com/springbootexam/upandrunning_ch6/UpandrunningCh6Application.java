f98ec1bab3a72a4726f2c049dc38bdf2688b9fcd
crow <justdoithaha@gmail.com>
2024-10-10 16:37:00 +0900

    up&running ch6 just started..

---
+++ src/main/java/com/springbootexam/upandrunning_ch6/UpandrunningCh6Application.java
@@ -0,0 +1,13 @@
+package com.springbootexam.upandrunning_ch6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpandrunningCh6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpandrunningCh6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootexam/upandrunning_ch6/UpandrunningCh6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootexam.upandrunning_ch6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpandrunningCh6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
